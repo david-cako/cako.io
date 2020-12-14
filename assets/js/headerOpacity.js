90743fb9d0907379900c90f26ff4fdaf42e02075
David Cako <dc@cako.io>
2020-12-13 18:57:13 -0700

    optimize headerOpacity.js

--- assets/js/headerOpacity.js
+++ assets/js/headerOpacity.js
@@ -1,5 +1,14 @@
+const cakoHeader = document.getElementById('cako-header-text');
+let lastOpacity = 1;
+
 const updateHeaderOpacity = () => {
-  document.getElementById('cako-header-text').style.opacity = 1 - (window.scrollY/50);
+  if (window.scrollY > 50 && lastOpacity === 0) {
+    return;
+  }
+  
+  const newOpacity = 1 - (window.scrollY/50)
+  cakoHeader.style.opacity = newOpacity;
+  lastOpacity = newOpacity; 
 }
 
 window.addEventListener('scroll', () => {
