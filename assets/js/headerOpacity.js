9dab03ac3fca0311fecd7fda1ccdf91a6ef7d509
David Cako <dc@cako.io>
2020-12-13 17:45:54 -0700

    header

--- assets/js/headerOpacity.js
+++ assets/js/headerOpacity.js
@@ -1,6 +1,12 @@
+const updateHeaderOpacity = () => {
+  document.getElementById('cako-header-text').style.opacity = 1 - (window.scrollY/50);
+}
+
 window.addEventListener('scroll', () => {
   if (window.scrollY>100) {
     return;
   }
-  document.getElementById('cako-header-text').style.opacity = 1 - (window.scrollY/50);
+  updateHeaderOpacity();
 });
+
+updateHeaderOpacity();
