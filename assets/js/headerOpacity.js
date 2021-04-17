afcc5729980049be09d5808c964ed4c95fb126e6
David Cako <dc@cako.io>
2021-04-17 12:50:47 -0600

    new theme

--- assets/js/headerOpacity.js
+++ assets/js/headerOpacity.js
@@ -1,20 +1,20 @@
 (() => {
   const cakoHeader = document.getElementById('cako-header-text');
-  const cakoHeaderMaxScroll = 50;
-  
+  const cakoHeaderMaxScroll = 40;
+
   let lastOpacity = 1;
 
   const updateHeaderOpacity = () => {
-    if (window.scrollY > 50 && lastOpacity === 0) {
+    if (window.scrollY > cakoHeaderMaxScroll && lastOpacity === 0) {
       return;
     }
 
-    const newOpacity = window.scrollY > cakoHeaderMaxScroll 
-      ? 0 
-      : 1 - (window.scrollY/cakoHeaderMaxScroll);
+    const newOpacity = window.scrollY > cakoHeaderMaxScroll
+      ? 0
+      : 1 - (window.scrollY / cakoHeaderMaxScroll);
 
     cakoHeader.style.opacity = newOpacity;
-    lastOpacity = newOpacity; 
+    lastOpacity = newOpacity;
   }
 
   window.addEventListener('scroll', () => {
