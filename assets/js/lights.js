3e4f4875753fe5cb8212ebcd003cce6f89629248
David Cako <dc@cako.io>
2021-09-01 04:24:56 -0600

    menu

--- assets/js/lights.js
+++ assets/js/lights.js
@@ -1,3 +1,27 @@
+function toggleLights() {
+    const lights = localStorage.getItem("lights");
+
+    if (lights === "off") {
+        const link = document.getElementById("dark-mode");
+
+        if (link) {
+            link.remove();
+        }
+
+        localStorage.setItem("lights", "on");
+    } else {
+        const link = document.createElement("link");
+        link.id = "dark-mode";
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = "/assets/css/dark.css";
+
+        document.head.appendChild(link);
+
+        localStorage.setItem("lights", "off");
+    }
+}
+
 (() => {
     const lights = localStorage.getItem("lights");
 
