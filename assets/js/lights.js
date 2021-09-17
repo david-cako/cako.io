a83b69ba4359c416045607802f4464c0b677a029
David Cako <dc@cako.io>
2021-09-17 17:11:29 -0600

    rev dark mode

--- assets/js/lights.js
+++ assets/js/lights.js
@@ -1,22 +1,30 @@
+function lightsOn() {
+    const link = document.getElementById("dark-mode");
+
+    if (link) {
+        link.remove();
+    }
+}
+
+function lightsOff() {
+    const link = document.createElement("link");
+    link.id = "dark-mode";
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "/assets/css/dark.2.css";
+
+    document.head.appendChild(link);
+}
+
 function toggleLights() {
     const lights = localStorage.getItem("lights");
 
     if (lights === "off") {
-        const link = document.getElementById("dark-mode");
-
-        if (link) {
-            link.remove();
-        }
+        lightsOn();
 
         localStorage.setItem("lights", "on");
     } else {
-        const link = document.createElement("link");
-        link.id = "dark-mode";
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        link.href = "/assets/css/dark.1.css";
-
-        document.head.appendChild(link);
+        lightsOff();
 
         localStorage.setItem("lights", "off");
     }
@@ -26,12 +34,6 @@ function toggleLights() {
     const lights = localStorage.getItem("lights");
 
     if (lights === "off") {
-        const link = document.createElement("link");
-        link.id = "dark-mode";
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        link.href = "/assets/css/dark.1.css";
-
-        document.head.appendChild(link);
+        lightsOff();
     }
 })();
