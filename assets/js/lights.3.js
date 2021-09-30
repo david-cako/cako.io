6ab1a54f6b7c1cfc42b567331848e9e4bc716bb5
David Cako <dc@cako.io>
2021-09-30 13:52:03 -0600

    link styles

--- assets/js/lights.3.js
+++ assets/js/lights.3.js
@@ -23,7 +23,7 @@ function lightsOff() {
     link.id = "dark-mode";
     link.type = "text/css";
     link.rel = "stylesheet";
-    link.href = "/assets/css/dark.2.css";
+    link.href = "/assets/css/dark.3.css";
 
     document.head.appendChild(link);
 
