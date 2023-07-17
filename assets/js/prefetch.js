4871208296780b705f27e9339f6d3b986514ca20
David Cako <dc@cako.io>
2023-07-17 12:11:55 -0600

    prefetch features page

--- assets/js/prefetch.js
+++ assets/js/prefetch.js
@@ -1,7 +1,7 @@
-(() => {
+function insertArticlePrefetchLinks(count = 10) {
     const articles = document.getElementsByClassName("cako-post-link");
 
-    const prefetchLinks = [...articles].slice(0, 10).map(article => {
+    const prefetchLinks = [...articles].slice(0, count).map(article => {
         const link = document.createElement("link");
         link.rel = "prefetch";
         link.href = article.href;
@@ -10,4 +10,17 @@
     });
 
     document.head.append(...prefetchLinks);
+}
+
+function insertFeaturesPrefetchLink() {
+    const link = document.createElement("link");
+    link.rel = "prefetch";
+    link.href = "/features/";
+
+    document.head.append(link);    
+}
+
+(() => {
+    insertArticlePrefetchLinks();
+    insertFeaturesPrefetchLink();
 })();
