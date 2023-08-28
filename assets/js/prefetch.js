86099e1bfd41cd461b866d3aabe4848445426e4c
David Cako <dc@cako.io>
2023-08-28 12:23:51 -0600

    prefetch first feature on load, clean up prefetch helpers

--- assets/js/prefetch.js
+++ assets/js/prefetch.js
@@ -1,59 +1,55 @@
-function insertArticlePrefetchLinks(count = 10) {
-    const articles = document.getElementsByClassName("cako-post-link");
-
-    const prefetchLinks = [...articles].slice(0, count).map(article => {
-        const link = document.createElement("link");
-        link.rel = "prefetch";
-        link.href = article.href;
-
-        return link;
-    });
-
-    document.head.append(...prefetchLinks);
-}
-
-function insertFeaturesPrefetchLink() {
+function insertPrefetchLink(url) {
     const link = document.createElement("link");
     link.rel = "prefetch";
-    link.href = "/features/";
+    link.href = url;
 
     document.head.append(link);
 }
 
+function insertArticlePrefetchLinks(count = 10) {
+    const articles = document.getElementsByClassName("cako-post-link");
+
+    [...articles].slice(0, count).map(article => {
+        insertPrefetchLink(article.href)
+    });
+}
+
+function insertFeaturesPrefetchLink() {
+    insertPrefetchLink("/features/");
+}
+
 function insertPostNavPrefetchLinks() {
     document.querySelectorAll(".post-nav-link").forEach(l => {
-        const link = document.createElement("link");
-        link.rel = "prefetch";
-        link.href = l.href;
-
-        document.head.append(link);
+        insertPrefetchLink(l.href)
     });
+}
+
+
+function insertPrefetchLinksForFeature(feature) {
+    const articles = feature.querySelectorAll(".cako-post-link");
+    articles.forEach(a => insertPrefetchLink(a.href));
 }
 
 /** Accepts article click event and adds pageshow handler to 
  * prefetch all sibling features after navigating back to "Features" 
  * from the destination article. */
-function prefetchFeatureArticles(e) {
-    const existing = [...document.head.querySelectorAll("link[rel='prefetch']")];
-    const existingPaths = existing.map(link => link.href);
-
-    const featureArticles = e.target.closest(".cako-featured")
-        .querySelectorAll(".cako-post-link");
-
-    const newLinks = [];
-
-    for (const a of featureArticles) {
-        if (!existingPaths.includes(a.href)) {
-            const link = document.createElement("link");
-            link.rel = "prefetch";
-            link.href = a.href;
-
-            newLinks.push(link);
-        }
-    }
-
+function prefetchFeatureArticlesOnPageShow(e) {
     function addPrefetchLinks() {
-        document.head.append(...newLinks);
+        const existing = [...document.head.querySelectorAll("link[rel='prefetch']")];
+        const existingPaths = existing.map(link => link.href);
+
+        const featureArticles = e.target.closest(".cako-featured")
+            .querySelectorAll(".cako-post-link");
+
+        const newLinks = [];
+
+        for (const a of featureArticles) {
+            if (!existingPaths.includes(a.href)) {
+                newLinks.push(a.href);
+            }
+        }
+
+        newLinks.forEach(insertPrefetchLink);
 
         window.removeEventListener("pageshow", addPrefetchLinks);
     };
@@ -61,12 +57,18 @@ function prefetchFeatureArticles(e) {
     window.addEventListener("pageshow", addPrefetchLinks)
 }
 
-/** Add click handlers to post links to prefetch all sibling features. */
-function setupFeaturePrefetchHandlers() {
+/** Add click handlers to post links to prefetch all sibling features,
+ * and prefetch articles for the first feature. */
+function initFeaturePrefetch() {
     const articles = document.getElementsByClassName("cako-post-link");
 
     for (const a of articles) {
-        a.addEventListener("click", prefetchFeatureArticles);
+        a.addEventListener("click", prefetchFeatureArticlesOnPageShow);
+    }
+
+    const firstFeature = document.querySelector(".cako-featured");
+    if (firstFeature) {
+        insertPrefetchLinksForFeature(firstFeature)
     }
 }
 
@@ -75,7 +77,7 @@ function setupFeaturePrefetchHandlers() {
         insertArticlePrefetchLinks();
         insertFeaturesPrefetchLink();
     } else if (window.location.pathname === "/features/") {
-        setupFeaturePrefetchHandlers();
+        initFeaturePrefetch();
     } else {
         insertPostNavPrefetchLinks();
     }
